58791bd78f949962817c540ef5002b92ebb30c35
Meridian74 <meridian@freemail.hu>
2019-02-23 15:45:21 +0100

    feat: first step

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var x = 0;
+var y = 0;
+var spacing = 20;
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+  background(255);
 }
 
 function draw() {
-  
+  stroke(0);
+  if (random(1) < 0.5) {
+    line(x, y, x + spacing, y + spacing);
+  }
+  else {
+    line(x, y + spacing, x + spacing, y);
+  }
+
 }
